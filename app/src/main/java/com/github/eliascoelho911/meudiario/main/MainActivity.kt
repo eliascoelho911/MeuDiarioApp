f7fe99cb79ad25f3b3b0175a0dede923660d51e7
eliascoelho911 <elias_coelho911@hotmail.com>
2022-04-18 17:17:07 -0300

    Initial commit

---
+++ app/src/main/java/com/github/eliascoelho911/meudiario/main/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.eliascoelho911.meudiario.main
+
+import androidx.appcompat.app.AppCompatActivity
+import com.github.eliascoelho911.meudiario.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ app/src/main/java/com/github/eliascoelho911/meudiario/main/MainApplication.kt
@@ -0,0 +1,6 @@
+package com.github.eliascoelho911.meudiario.main
+
+import android.app.Application
+
+class MainApplication : Application() {
+}
